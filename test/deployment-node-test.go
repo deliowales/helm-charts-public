ce25ddc060489235f2c739834e0d5eca4ee6fa81
Alex Le Peltier <alp@deliowealth.com>
2022-08-16 15:54:36 +0100

    Fixing issue with int32 pointer not casting to integer

--- test/deployment-node-test.go
+++ test/deployment-node-test.go
@@ -1,41 +1,32 @@
 package test
 
 import (
-        "testing"
-
-        "strconv"
-
-        appsv1 "k8s.io/api/apps/v1"
-
-        "github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/helm"
+	appsv1 "k8s.io/api/apps/v1"
+	"testing"
 )
 
-func TestdeploymentTemplate(t *testing.T) {
-        // Path to the helm chart we will test
-        helmChartPath := "../charts/microservice"
-        // Setup the args. For this test, we will set the following input values:
-        options := &helm.Options{
-            SetValues: map[string]string{
-        			"application.name": "testapp",
-        			"application.env": "uat",
-        			"cloud.region": "eu-west-1",
-        			"application.resources.limits.memory": "100mi",
-        			"application.resources.requests.memory": "100mi",
-        	},
-        }
+func TestNodeDeploymentTemplate(t *testing.T) {
+	helmChartPath := "../charts/microservice"
 
-        // Run RenderTemplate to render the template and capture the output.
-        output := helm.RenderTemplate(t, options, helmChartPath, "deployment-node", []string{"templates/deployment-node.yaml"})
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"application.name":                      "testapp",
+			"application.env":                       "uat",
+			"cloud.region":                          "eu-west-1",
+			"application.resources.limits.memory":   "100mi",
+			"application.resources.requests.memory": "100mi",
+		},
+	}
 
-        // Now we use kubernetes/client-go library to render the template output into the deployment struct. This will
-        // ensure the deployment resource is rendered correctly.
-        var deploymentnode appsv1.Deployment
-        helm.UnmarshalK8SYaml(t, output, &deploymentnode)
+	output := helm.RenderTemplate(t, options, helmChartPath, "deployment-node", []string{"templates/deployment-node.yaml"})
 
-        // Finally, we verify the deployment spec is set to the expected value
-        expectedReplicas := "3"
-        Replicas := strconv.Itoa(deploymentnode.Spec.Replicas)
-        if Replicas != expectedReplicas {
-            t.Fatalf("Rendered replica count (%s) is not expected (%s)", Replicas, expectedReplicas)
-        }
+	var deploymentnode appsv1.Deployment
+	helm.UnmarshalK8SYaml(t, output, &deploymentnode)
+
+	expectedReplicas := 3
+	replicas := deploymentnode.Spec.Replicas
+	if int(*replicas) != expectedReplicas {
+		t.Fatalf("Rendered replica count (%s) is not expected (%s)", replicas, expectedReplicas)
+	}
 }
--- test/ingress-test.go
+++ test/ingress-test.go
@@ -1,37 +1,37 @@
 package test
 
 import (
-        "testing"
+	"testing"
 
-        networkingv1 "k8s.io/api/networking/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 
-        "github.com/gruntwork-io/terratest/modules/helm"
+	"github.com/gruntwork-io/terratest/modules/helm"
 )
 
 func TestIngressTemplate(t *testing.T) {
-        // Path to the helm chart we will test
-        helmChartPath := "../charts/microservice"
-        // Setup the args. For this test, we will set the following input values:
-        options := &helm.Options{
-            SetValues: map[string]string{
-        			"application.name": "testapp",
-        			"ingress.enabled": "true",
-        			"ingress.path": "/testapp",
-        			"ingress.pathrouted": "/kong/testapp",
-        	},
-        }
-        // Run RenderTemplate to render the template and capture the output.
-        output := helm.RenderTemplate(t, options, helmChartPath, "ingress", []string{"templates/ingress.yaml"})
+	// Path to the helm chart we will test
+	helmChartPath := "../charts/microservice"
+	// Setup the args. For this test, we will set the following input values:
+	options := &helm.Options{
+		SetValues: map[string]string{
+			"application.name":   "testapp",
+			"ingress.enabled":    "true",
+			"ingress.path":       "/testapp",
+			"ingress.pathrouted": "/kong/testapp",
+		},
+	}
+	// Run RenderTemplate to render the template and capture the output.
+	output := helm.RenderTemplate(t, options, helmChartPath, "ingress", []string{"templates/ingress.yaml"})
 
-        // Now we use kubernetes/client-go library to render the template output into the deployment struct. This will
-        // ensure the deployment resource is rendered correctly.
-        var ingress networkingv1.Ingress
-        helm.UnmarshalK8SYaml(t, output, &ingress)
+	// Now we use kubernetes/client-go library to render the template output into the deployment struct. This will
+	// ensure the deployment resource is rendered correctly.
+	var ingress networkingv1.Ingress
+	helm.UnmarshalK8SYaml(t, output, &ingress)
 
-        // Finally, we verify the ingress spec is set to the expected value
-        expectedPath := "/testapp"
-        Path := ingress.Spec.Rules.Http.Paths.Path
-        if Path != expectedPath {
-            t.Fatalf("Rendered path (%s) is not expected (%s)", Path, expectedPath)
-        }
+	// Finally, we verify the ingress spec is set to the expected value
+	expectedPath := "/testapp"
+	Path := ingress.Spec.Rules.Http.Paths.Path
+	if Path != expectedPath {
+		t.Fatalf("Rendered path (%s) is not expected (%s)", Path, expectedPath)
+	}
 }
