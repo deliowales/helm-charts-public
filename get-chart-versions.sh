4496115b4d7beaf1beda7da9dcde984343c1321e
Adam Yaziji <ay@adamyaziji.com>
2023-01-12 12:51:22 +0000

    adding workflow job to get chart badge

--- get-chart-versions.sh
+++ get-chart-versions.sh
@@ -2,7 +2,8 @@
 echo "# Charts" > README.md
 echo "Updating Chart version badges in README..."
 for d in charts/*/ ; do
-  version=$(grep -oP '(?<=version\:\s)[^ ]*' $d/Chart.yaml | tr -d \'\" )
-  chart=${d#*/}
-  echo "## [$(echo $chart | tr -d "/")]($d) ![Version: $version](https://img.shields.io/badge/Version-$version-informational?style=flat-square)" >> README.md
+  version=$(ggrep -oP '(?<=version\:\s)[^ ]*' $d/Chart.yaml | tr -d \'\" )
+  chart=$( echo ${d#*/} | tr -d "/")
+  echo $chart
+  echo "## [${chart^}]($d): ![Version: $version](https://img.shields.io/badge/Version-$version-informational?style=flat-square)" >> README.md
 done
